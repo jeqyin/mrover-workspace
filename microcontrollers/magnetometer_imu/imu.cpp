6bca4ee3d14475bf48ff6a544e44713f800017d5
Nathan Moos <moosingin3space@gmail.com>
2017-11-29 12:02:38 -0500

    [microcontrollers] code now works

--- microcontrollers/magnetometer_imu/imu.cpp
+++ microcontrollers/magnetometer_imu/imu.cpp
@@ -2,34 +2,63 @@
 
 Imu::Imu(PinName sda, PinName scl) :
     i2c_(sda, scl),
-    valid_(false)
+    valid_(false),
+    check_valid_(false)
 {
+    init();
+}
+
+void Imu::init() {
     memset(&this->accel_, 0, sizeof(this->accel_));
     memset(&this->mag_, 0, sizeof(this->mag_));
+    memset(&this->gyro_, 0, sizeof(this->gyro_));
+
+    this->check_valid_ = false;
+    // -- Initialize Accelerometer/Magnetometer --
     // Ensure that it's properly connected
-    uint8_t id = this->read_reg(REGISTER_WHO_AM_I);
-    if (id != ID) {
+    uint8_t id = this->read_am_reg(AM_REGISTER_WHO_AM_I);
+    if (id != AM_ID) {
+        this->check_valid_ = true;
         return; // valid_ = false
     }
 
     // Set the IMU to standby
-    this->write_reg(REGISTER_CTRL_REG1, 0);
+    this->write_am_reg(AM_REGISTER_CTRL_REG1, 0);
 
+    // Configure accelerometer
     // TODO make the range customizable (rn hard-coded for 4G)
-    this->write_reg(REGISTER_XYZ_DATA_CFG, 0x01);
-
+    this->write_am_reg(AM_REGISTER_XYZ_DATA_CFG, 0x01);
     // Enable high-resolution mode
-    this->write_reg(REGISTER_CTRL_REG2, 0x02);
+    this->write_am_reg(AM_REGISTER_CTRL_REG2, 0x02);
     // Active, Normal, Low-noise, 100 Hz, hybrid
-    this->write_reg(REGISTER_CTRL_REG1, 0x15);
+    this->write_am_reg(AM_REGISTER_CTRL_REG1, 0x15);
 
     // Configure magnetometer
     // Hybrid, oversampling rate=16
-    this->write_reg(REGISTER_MCTRL_REG1, 0x1F);
+    this->write_am_reg(AM_REGISTER_MCTRL_REG1, 0x1F);
     // Jump to reg 0x33 after reading 0x06
-    this->write_reg(REGISTER_MCTRL_REG2, 0x20);
+    this->write_am_reg(AM_REGISTER_MCTRL_REG2, 0x20);
+
+    // -- Initialize Gyroscope --
+    // Ensure that it's properly connected
+    id = this->read_gy_reg(GY_REGISTER_WHO_AM_I);
+    if (id != GY_ID) {
+        this->check_valid_ = true;
+        return; // valid_ = false
+    }
+
+    // Set standby
+    this->write_gy_reg(GY_REGISTER_CTRL_REG1, 0x00);
+    // Reset gyro
+    this->write_gy_reg(GY_REGISTER_CTRL_REG1, (1<<6));
+    // Set sensitivity to 1000 dps TODO configurable
+    this->write_gy_reg(GY_REGISTER_CTRL_REG1, 0x01);
+    // Set active
+    this->write_gy_reg(GY_REGISTER_CTRL_REG1, 0x0E);
+    wait(0.1);
 
     this->valid_ = true;
+    this->check_valid_ = true;
 }
 
 bool Imu::valid() const {
@@ -39,9 +68,10 @@ bool Imu::valid() const {
 void Imu::read() {
     memset(&this->accel_, 0, sizeof(this->accel_));
     memset(&this->mag_, 0, sizeof(this->mag_));
+    memset(&this->gyro_, 0, sizeof(this->gyro_));
 
-    char msg = REGISTER_STATUS | 0x80;
-    this->i2c_.write(ADDRESS, &msg, 1);
+    char msg = AM_REGISTER_STATUS | 0x80;
+    this->i2c_.write(AM_ADDRESS, &msg, 1);
 
     /*
      *  [0] = status
@@ -58,16 +88,35 @@ void Imu::read() {
      * [11] = Magnetometer Z high byte
      * [12] = Magnetometer Z low byte
      */
-    char recvBuf[13];
-    this->i2c_.read(ADDRESS, recvBuf, 13);
+    char am_recv_buf[13];
+    this->i2c_.read(AM_ADDRESS, am_recv_buf, 13);
 
-    int16_t accel_x = (int16_t)((recvBuf[1] << 8) | recvBuf[2]) >> 2;
-    int16_t accel_y = (int16_t)((recvBuf[3] << 8) | recvBuf[4]) >> 2;
-    int16_t accel_z = (int16_t)((recvBuf[5] << 8) | recvBuf[6]) >> 2;
+    msg = GY_REGISTER_STATUS | 0x80;
+    this->i2c_.write(GY_ADDRESS, &msg, 1);
 
-    int16_t mag_x = (int16_t)((recvBuf[7] << 8) | recvBuf[8]);
-    int16_t mag_y = (int16_t)((recvBuf[9] << 8) | recvBuf[10]);
-    int16_t mag_z = (int16_t)((recvBuf[11] << 8) | recvBuf[12]);
+    /*
+     * [0] = status
+     * [1] = Gyroscope X high byte
+     * [2] = Gyroscope X low byte
+     * [3] = Gyroscope Y high byte
+     * [4] = Gyroscope Y low byte
+     * [5] = Gyroscope Z high byte
+     * [6] = Gyroscope Z low byte
+     */
+    char gy_recv_buf[7];
+    this->i2c_.read(GY_ADDRESS, gy_recv_buf, 7);
+
+    int16_t accel_x = (int16_t)((am_recv_buf[1] << 8) | am_recv_buf[2]) >> 2;
+    int16_t accel_y = (int16_t)((am_recv_buf[3] << 8) | am_recv_buf[4]) >> 2;
+    int16_t accel_z = (int16_t)((am_recv_buf[5] << 8) | am_recv_buf[6]) >> 2;
+
+    int16_t mag_x = (int16_t)((am_recv_buf[7] << 8) | am_recv_buf[8]);
+    int16_t mag_y = (int16_t)((am_recv_buf[9] << 8) | am_recv_buf[10]);
+    int16_t mag_z = (int16_t)((am_recv_buf[11] << 8) | am_recv_buf[12]);
+
+    int16_t gyro_x = (int16_t)((gy_recv_buf[1] << 8) | gy_recv_buf[2]);
+    int16_t gyro_y = (int16_t)((gy_recv_buf[3] << 8) | gy_recv_buf[4]);
+    int16_t gyro_z = (int16_t)((gy_recv_buf[5] << 8) | gy_recv_buf[6]);
 
     this->accel_.x = accel_x * ACCEL_MG_LSB_4G * GRAVITY_STANDARD;
     this->accel_.y = accel_y * ACCEL_MG_LSB_4G * GRAVITY_STANDARD;
@@ -76,6 +125,10 @@ void Imu::read() {
     this->mag_.x = mag_x * MAG_UT_LSB;
     this->mag_.y = mag_y * MAG_UT_LSB;
     this->mag_.z = mag_z * MAG_UT_LSB;
+
+    this->gyro_.x = gyro_x * GYRO_SENSITIVITY_1000DPS;
+    this->gyro_.y = gyro_y * GYRO_SENSITIVITY_1000DPS;
+    this->gyro_.z = gyro_z * GYRO_SENSITIVITY_1000DPS;
 }
 
 Vector3f Imu::accelerometer() const {
@@ -86,13 +139,41 @@ Vector3f Imu::magnetometer() const {
     return this->mag_;
 }
 
-void Imu::write_reg(uint8_t reg, uint8_t value) {
-    char buf[2] = {reg, value};
-    this->i2c_.write(ADDRESS, buf, 2);
+Vector3f Imu::gyroscope() const {
+    return this->gyro_;
 }
 
-uint8_t Imu::read_reg(uint8_t reg) {
+void Imu::write_am_reg(uint8_t reg, uint8_t value) {
+    this->write_reg(AM_ADDRESS, reg, value);
+}
+
+uint8_t Imu::read_am_reg(uint8_t reg) {
+    return this->read_reg(AM_ADDRESS, reg);
+}
+
+void Imu::write_gy_reg(uint8_t reg, uint8_t value) {
+    this->write_reg(GY_ADDRESS, reg, value);
+}
+
+uint8_t Imu::read_gy_reg(uint8_t reg) {
+    return this->read_reg(GY_ADDRESS, reg);
+}
+
+void Imu::write_reg(uint8_t addr, uint8_t reg, uint8_t value) {
+    if (this->check_valid_ && !this->valid_) return;
+    char buf[2] = {reg, value};
+    if (this->i2c_.write(addr, buf, 2) != 0) {
+        this->valid_ = false;
+    }
+}
+
+uint8_t Imu::read_reg(uint8_t addr, uint8_t reg) {
+    if (this->check_valid_ && !this->valid_) return 255;
     char signed_reg = (char) reg;
-    this->i2c_.write(ADDRESS, &signed_reg, 1);
-    return (uint8_t)this->i2c_.read(ADDRESS);
+    this->i2c_.write(addr, &signed_reg, 1, true);
+    char value;
+    if (this->i2c_.read(addr, &value, 1) != 0) {
+        this->valid_ = false;
+    }
+    return (uint8_t) value;
 }
--- microcontrollers/magnetometer_imu/main.cpp
+++ microcontrollers/magnetometer_imu/main.cpp
@@ -1,16 +1,32 @@
 #include "mbed.h"
 #include "imu.hpp"
 
+DigitalOut green_led(LED1);
+DigitalOut red_led(LED2);
 Serial dbg(USBTX, USBRX, 9600);
 Imu imu(PB_7, PB_6);
 
 int main() {
     while (true) {
+        if (!imu.valid()) {
+            green_led = false;
+            red_led = true;
+            dbg.printf("invalid IMU, check connection\r\n");
+            imu.init();
+            dbg.printf("reconnecting...\r\n");
+            wait(1.0);
+            continue;
+        } else {
+            red_led = false;
+            green_led = true;
+        }
         imu.read();
         Vector3f a = imu.accelerometer();
         Vector3f m = imu.magnetometer();
-        dbg.printf("A: (x=%.4f, y=%.4f, z=%.4f) m/s^2\n", a.x, a.y, a.z);
-        dbg.printf("M: (x=%.4f, y=%.4f, z=%.4f) uT\n", m.x, m.y, m.z);
+        Vector3f g = imu.gyroscope();
+        dbg.printf("A: (x=%.4f, y=%.4f, z=%.4f) m/s^2\r\n", a.x, a.y, a.z);
+        dbg.printf("M: (x=%.4f, y=%.4f, z=%.4f) uT\r\n", m.x, m.y, m.z);
+        dbg.printf("G: (x=%.4f, y=%.4f, z=%.4f) deg/s\r\n", g.x, g.y, g.z);
 
         wait(0.5);
     }
